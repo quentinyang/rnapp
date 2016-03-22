575d12c96d556277521dbc403db875abb70e19e7
杨文锦 <quentin.yang@angejia.com>
2016-03-22 18:24:52 +0800

    set code push@android

--- android/app/src/main/java/com/fy360/MainActivity.java
+++ android/app/src/main/java/com/fy360/MainActivity.java
@@ -4,10 +4,35 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
+// 1. Import the plugin class
+import com.microsoft.codepush.react.CodePush;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends ReactActivity {
+
+    // 2. Define a private field to hold the CodePush runtime instance
+    private CodePush _codePush;
+
+    // 3. Override the getJSBundleFile method in order to let
+    // the CodePush runtime determine where to get the JS
+    // bundle location from on each app start
+    @Override
+    protected String getJSBundleFile() {
+        return this._codePush.getBundleUrl("index.android.bundle");
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+        // 4. Instantiate an instance of the CodePush runtime, using the right deployment key. If you don't
+        // already have it, you can run "code-push deployment ls <appName> -k" to retrieve your key.
+        this._codePush = new CodePush("Oj0a7yqg1CpDnMfpKp-7O3aZZ_US4yoltiYTl", this, BuildConfig.DEBUG);
+
+        // 5. Add the CodePush package to the list of existing packages
+        return Arrays.<ReactPackage>asList(
+            new MainReactPackage(), this._codePush.getReactPackage());
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
