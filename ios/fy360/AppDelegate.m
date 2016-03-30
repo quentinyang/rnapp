ad64ff28a081d4a43dd9e1bf8b965d5905815039
杨文锦 <quentin.yang@angejia.com>
2016-03-30 21:11:13 +0800

    Integrate GeTui

--- ios/fy360/AppDelegate.m
+++ ios/fy360/AppDelegate.m
@@ -13,6 +13,10 @@
 
 #import "CodePush.h"
 
+NSString *const NotificationCategoryIdent = @"ACTIONABLE";
+//NSString *const NotificationActionOneIdent = @"ACTION_ONE";
+//NSString *const NotificationActionTwoIdent = @"ACTION_TWO";
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -55,7 +59,7 @@
 //   jsCodeLocation = [CodePush bundleURLForResource:@"index.ios" withExtension:@"jsbundle"];
 
   #ifdef DEBUG
-    jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+    jsCodeLocation = [NSURL URLWithString:@"http://192.168.164.87:8081/index.ios.bundle?platform=ios&dev=true"];
   #else
     jsCodeLocation = [CodePush bundleURLForResource:@"index.ios" withExtension:@"jsbundle"];
   #endif
@@ -70,7 +74,165 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  
+  // [1]:使用APPID/APPKEY/APPSECRENT创建个推实例
+  // 该方法需要在主线程中调用
+  [self startSdkWith:kGtAppId appKey:kGtAppKey appSecret:kGtAppSecret];
+  
+  // [2]:注册APNS
+  [self registerRemoteNotification];
+  
+  // [2-EXT]: 获取启动时收到的APN数据
+  NSDictionary *userInfo = [launchOptions objectForKey:UIApplicationLaunchOptionsRemoteNotificationKey];
+  if (userInfo) {
+    NSString *record = [NSString stringWithFormat:@"[APN]%@, %@", [NSDate date], userInfo];
+    NSLog(@"\n>>>[GeTuiSdk DeviceToken Success]:%@\n\n", record);
+  }
+  
   return YES;
+}
+
+#pragma mark - 用户通知(推送) _自定义方法
+
+/** 注册APNS,注:获取DeviceToken不同项目或版本会有所不同，可以参考如下方式注册APNs。 */
+/** 注册远程通知 */
+- (void)registerRemoteNotification {
+#ifdef __IPHONE_8_0
+  if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0) {
+    //IOS8 新的通知机制category注册
+    UIMutableUserNotificationCategory *actionCategory;
+    actionCategory = [[UIMutableUserNotificationCategory alloc] init];
+    [actionCategory setIdentifier:NotificationCategoryIdent];
+    [actionCategory setActions:nil
+                    forContext:UIUserNotificationActionContextDefault];
+    
+    NSSet *categories = [NSSet setWithObject:actionCategory];
+    UIUserNotificationType types = (UIUserNotificationTypeAlert |
+                                    UIUserNotificationTypeSound |
+                                    UIUserNotificationTypeBadge);
+    
+    UIUserNotificationSettings *settings;
+    settings = [UIUserNotificationSettings settingsForTypes:types categories:categories];
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
+    [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+    
+  } else {
+    UIRemoteNotificationType apn_type = (UIRemoteNotificationType)(UIRemoteNotificationTypeAlert |
+                                                                   UIRemoteNotificationTypeSound |
+                                                                   UIRemoteNotificationTypeBadge);
+    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:apn_type];
+  }
+#else
+  UIRemoteNotificationType apn_type = (UIRemoteNotificationType)(UIRemoteNotificationTypeAlert |
+                                                                 UIRemoteNotificationTypeSound |
+                                                                 UIRemoteNotificationTypeBadge);
+  [[UIApplication sharedApplication] registerForRemoteNotificationTypes:apn_type];
+#endif
+}
+
+#pragma mark - 远程通知(推送)回调
+
+/** 远程通知注册成功委托 */
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+  NSString *token = [[deviceToken description] stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]];
+  token = [token stringByReplacingOccurrencesOfString:@" " withString:@""];
+  NSLog(@"\n>>>[GeTuiSdk DeviceToken Success]:%@\n\n", token);
+  
+  // [3]:向个推服务器注册deviceToken
+  [GeTuiSdk registerDeviceToken:token];
+}
+
+/** 远程通知注册失败委托 */
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+//  [UIViewController logMsg:[NSString stringWithFormat:@"didFailToRegisterForRemoteNotificationsWithError:%@", [error localizedDescription]]];
+  NSLog(@"\n>>>[GeTuiSdk DeviceToken Error]:%s\n\n", "APNS注册失败");
+}
+
+#pragma mark - background fetch  唤醒
+- (void)application:(UIApplication *)application performFetchWithCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+  
+  //[5] Background Fetch 恢复SDK 运行
+  [GeTuiSdk resume];
+  
+  completionHandler(UIBackgroundFetchResultNewData);
+}
+
+#pragma mark - APP运行中接收到通知(推送)处理
+
+/** APP已经接收到“远程”通知(推送) - (App运行在后台/App运行在前台)  */
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult result))completionHandler {
+  
+  // [4-EXT]:处理APN
+  NSString *record = [NSString stringWithFormat:@"[APN]%@, %@", [NSDate date], userInfo];
+  NSLog(@"\n>>>[GeTuiSdk DidAPN]:%@\n\n", record);
+  
+  completionHandler(UIBackgroundFetchResultNewData);
+}
+
+/******************* Demo ***********************/
+
+#pragma mark - 启动GeTuiSdk
+
+- (void)startSdkWith:(NSString *)appID appKey:(NSString *)appKey appSecret:(NSString *)appSecret {
+  //[1-1]:通过 AppId、 appKey 、appSecret 启动SDK
+  //该方法需要在主线程中调用
+  [GeTuiSdk startSdkWithAppId:appID
+                       appKey:appKey appSecret:appSecret delegate:self];
+  
+  //[1-2]:设置是否后台运行开关
+  [GeTuiSdk runBackgroundEnable:YES];
+  //[1-3]:设置电子围栏功能，开启LBS定位服务 和 是否允许SDK 弹出用户定位请求
+  [GeTuiSdk lbsLocationEnable:YES andUserVerify:YES];
+}
+
+#pragma mark - GeTuiSdkDelegate
+
+/** SDK启动成功返回cid */
+- (void)GeTuiSdkDidRegisterClient:(NSString *)clientId {
+  //个推SDK已注册，返回clientId
+  NSLog(@"\n>>>[GeTuiSdk RegisterClient]:%@\n\n", clientId);
+}
+
+
+/** SDK收到透传消息回调 */
+- (void)GeTuiSdkDidReceivePayloadData:(NSData *)payloadData andTaskId:(NSString *)taskId andMsgId:(NSString *)msgId andOffLine:(BOOL)offLine fromGtAppId:(NSString *)appId {
+  // [4]: 收到个推消息
+  NSString *payloadMsg = nil;
+  if (payloadData) {
+    payloadMsg = [[NSString alloc] initWithBytes:payloadData.bytes
+                                          length:payloadData.length
+                                        encoding:NSUTF8StringEncoding];
+  }
+  
+  NSString *record = [NSString stringWithFormat:@"%d, %@, %@%@", ++_lastPayloadIndex, [self formateTime:[NSDate date]], payloadMsg, offLine ? @"<离线消息>" : @""];
+  NSLog(@"\n>>>[GexinSdk ReceivePayload Record]:%@\n\n", record);
+  
+  NSString *msg = [NSString stringWithFormat:@"%@ : %@%@", [self formateTime:[NSDate date]], payloadMsg, offLine ? @"<离线消息>" : @""];
+  NSLog(@"\n>>>[GexinSdk ReceivePayload Message]:%@, taskId: %@, msgId :%@", msg, taskId, msgId);
+  
+  // 汇报个推自定义事件
+  [GeTuiSdk sendFeedbackMessage:90001 taskId:taskId msgId:msgId];
+}
+
+/** SDK收到sendMessage消息回调 */
+- (void)GeTuiSdkDidSendMessage:(NSString *)messageId result:(int)result {
+  // [4-EXT]:发送上行消息结果反馈
+  NSString *record = [NSString stringWithFormat:@"Received sendmessage:%@ result:%d", messageId, result];
+  NSLog(@"\n>>>[GexinSdk Callback Success]:%@\n\n", record);
+}
+
+/** SDK遇到错误回调 */
+- (void)GeTuiSdkDidOccurError:(NSError *)error {
+  //个推错误报告，集成步骤发生的任何错误都在这里通知，如果集成后，无法正常收到消息，查看这里的通知。
+  NSLog(@"\n>>>[GexinSdk Callback Error]:%@\n\n", [error localizedDescription]);
+}
+
+- (NSString *)formateTime:(NSDate *)date {
+  NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+  [formatter setDateFormat:@"HH:mm:ss"];
+  NSString *dateTime = [formatter stringFromDate:date];
+  return dateTime;
 }
 
 @end
