0e0576eb2bda4a538e9806f6221a4c5a20060f27
周晓建 <jameszhou@angejia.com>
2016-03-31 21:20:37 +0800

    修改url

--- app/config/_urls_debug.js
+++ app/config/_urls_debug.js
@@ -1,6 +1,7 @@
 'use strict';
 
-let HOST = 'http://192.168.164.112:8081/app/fakedata/';
+let HOST = 'http://192.168.163.101:8081/app/fakedata/';
+let HOST1 = 'http://360.feature-fy360.dev.angejia.com/service/';
 
 let urls = {
     test: {
@@ -10,8 +11,10 @@ let urls = {
         baseInfo: HOST + 'property-detail.json'
     },
     user: {
-        sendCode: HOST + 'sendcode.json',
-        login: HOST + 'login.json'
+        // sendCode: HOST1 + 'sendcode.json',
+        // login: HOST1 + 'login.json',
+        sendCode: HOST1 + 'passport/login/sms',
+        login: HOST1 + 'passport/login'
     },
     house: {
         list: HOST + 'house-list.json',
@@ -19,7 +22,16 @@ let urls = {
         prepend: HOST + 'house-prepend.json',
         attention: HOST + 'house-attention.json',
         attentionAppend: HOST + 'house-attention-append.json',
-        attentionPrepend: HOST + 'house-attention-prepend.json'
+        attentionPrepend: HOST + 'house-attention-prepend.json',
+        similar: HOST + 'house-similar.json'
+    },
+    blocks: {
+        attention: HOST1 + 'get-attention-block',
+        saveAttention: HOST1 + 'set-attention-block'
+    },
+    attention: {
+        blockAndCommunity: HOST1 + 'get-attention-community-block',
+        saveAttentionCommunity: HOST1 + 'set-attention-community'
     }
 }
 
