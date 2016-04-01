e06a690730389fc1058304b6e751e68d0ce3561c
杨文锦 <quentin.yang@angejia.com>
2016-04-01 19:39:27 +0800

    add codepush commented by James

--- index.ios.js
+++ index.ios.js
@@ -2,15 +2,13 @@
 
 import {React, Component, AppRegistry, StyleSheet} from 'nuke';
 import Root from './app/root';
-// import codePush from "react-native-code-push";
+import codePush from "react-native-code-push";
 
 class fy360 extends Component {
     componentDidMount() {
         // download silently and installed the next time the app is restarted
-        // codePush.sync({rollbackTimeout: 3000});
+        codePush.sync({rollbackTimeout: 3000});
         // Prompt the user when an update is available,
-        // codePush.sync({rollbackTimeout: 3000});
-        // Prompt the user when an update is available, 
         // if aggree, and then display a "downloading" modal, and update immediately
         // codePush.sync({updateDialog: true, installMode: codePush.InstallMode.IMMEDIATE, rollbackTimeout: 3000});
     }
