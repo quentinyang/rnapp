df384f677bbaea83766b07b51ecdf3ec4f42cef5
杨文锦 <quentin.yang@angejia.com>
2016-04-07 19:36:56 +0800

    add header in ajax

--- app/utils/Ajax.js
+++ app/utils/Ajax.js
@@ -1,14 +1,64 @@
 'use strict';
 
+import deviceInfo from './DeviceInfo';
+
 function status(response, resolve, reject) {
     if((response.status >= 200 && response.status < 300) || response.status == 304) {
         resolve(response.json());
     } else {
-        if (response.status == 400) {
+        if (response.status == 401) {
+            console.log('[Ajax Error] 401');
             // TODO::go to login
         }
         response.json().then(reject);
     }
+}
+
+var mobileAgent = (function() {
+        var infos = {
+            app: 'FangYuan360',//app APP 名称
+            av: deviceInfo.version,//APP 版本号
+            ccid: '1',//用户选择的城市 id
+            gcid: '1',//定位到的城市 id
+            ch: '',//发布的渠道 id
+            lng: '',//经度
+            lat: '',//纬度
+            ip: '',
+            net: '',//网络类型
+            p: (deviceInfo.manufacturer == 'Apple') ? 'IOS' : 'Android',//Android or IOS
+            pm: deviceInfo.manufacturer,//设备厂家或类型（三星，iPod, Touch)
+            osv: deviceInfo.systemVersion,//操作系统版本
+            // dvid 设备ID(DeviceID)，安卓:用IMEI+Mac地址表示，e.g.:447769804451095+c5ab204dc58739c482f413d2a22de442；
+            // IOS:用udid表示，举例:8A9B2903-B0D8-4137-A8BC-5C7F32CE9F04
+            dvid: deviceInfo.uniqueId,
+            // idfa optional, iOS 广告标志符，android 无此字段
+            idfa: ''
+        };
+
+        var agent = [];
+        for (let index in infos) {
+            agent.push(index + '=' + infos[index]);
+        }
+        return agent.join(';');
+
+})();
+
+function _getHeader() {
+    // Refer to: https://git.corp.angejia.com/service/design/wikis/Mobile-API/Spec
+    var header = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'token': global.gtoken || '',
+            'Angejia-Signature': '',// 签名信息
+            'Angejia-Auth': global.gtoken,// 用户认证信息
+            'Angejia-MobileAgent': mobileAgent,// 移动设备信息
+            'Angejia-CamelCase': '1',// JSON 数据使用驼峰形式的键
+            'Angejia-Stringify': '1',// JSON 数据使用字符串形式的值
+            'Angejia-Env': '',// 移动应用访问的 Mobile API 环境
+            'Angejia-Payload': '',// 返回内容结构（兼容设置）
+    }
+    console.log('Http Header', header);
+    return header;
 }
 
 function _createURL(url, params) {
@@ -28,9 +78,7 @@ function get(url, params = {}) {
     return new Promise((resolve, reject) => {
         fetch(_createURL(url, data || {}), {
             method: 'GET',
-            headers: {
-                token: gtoken
-            },
+            headers: _getHeader(),
             ...paramsOther
         })
         .then((response) => {
@@ -48,11 +96,7 @@ function post(url, params = {}) {
     return new Promise((resolve, reject) => {
         fetch(url, {
             method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'token': gtoken
-            },
+            headers: _getHeader(),
             body: JSON.stringify(body),
             ...paramsOther
         })
---
+++ app/utils/DeviceInfo.js
@@ -0,0 +1,21 @@
+var DeviceInfo = require('react-native-device-info');
+
+var deviceInfo = {
+    // * note this is IDFV on iOS so it will change if all apps from the current apps vendor have been previously uninstalled
+    uniqueId: DeviceInfo.getUniqueID(),  // e.g. FCDBD8EF-62FC-4ECB-B2F5-92C9E79AC7F9
+    manufacturer: DeviceInfo.getManufacturer(),  // e.g. Apple
+    model: DeviceInfo.getModel(),  // e.g. iPhone 6
+    deviceId: DeviceInfo.getDeviceId(),  // e.g. iPhone7.2 / or the board on Android e.g. goldfish
+    systemName: DeviceInfo.getSystemName(),  // e.g. iPhone OS
+    systemVersion: DeviceInfo.getSystemVersion(),  // e.g. 9.0
+    bundleId: DeviceInfo.getBundleId(),  // e.g. com.learnium.mobile
+    buildNum: DeviceInfo.getBuildNumber(),  // e.g. 89
+    version: DeviceInfo.getVersion(),  // e.g. 1.1.0
+    readableVersion: DeviceInfo.getReadableVersion(),  // e.g. 1.1.0.89
+    deviceName: DeviceInfo.getDeviceName(),  // e.g. Becca's iPhone 6
+    userAgent: DeviceInfo.getUserAgent(), // e.g. Dalvik/2.1.0 (Linux; U; Android 5.1; Google Nexus 4 - 5.1.0
+    deviceLocale: DeviceInfo.getDeviceLocale(), // e.g en-US
+    deviceCountry: DeviceInfo.getDeviceCountry(), // e.g US
+};
+
+export default deviceInfo;
