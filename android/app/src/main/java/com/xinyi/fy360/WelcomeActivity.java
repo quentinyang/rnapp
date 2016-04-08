9a395c58deec42219c3e64f772536b3c8346eba1
杨文锦 <quentin.yang@angejia.com>
2016-04-08 21:40:41 +0800

    refine welcome for android

--- android/app/src/main/java/com/xinyi/fy360/WelcomeActivity.java
+++ android/app/src/main/java/com/xinyi/fy360/WelcomeActivity.java
@@ -1,42 +1,31 @@
 package com.xinyi.fy360;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.util.Log;
-import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
+import android.os.Handler;
+import android.os.Message;
 
-// Import Getui
-import com.igexin.sdk.PushManager;
+/**
+ * Created by quentin on 16-04-08.
+ */
+public class WelcomeActivity extends Activity{
 
-// 1. Import the plugin class
-import com.microsoft.codepush.react.CodePush;
-
-import java.util.Arrays;
-import java.util.List;
-
-// Import react native device info
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-
-public class MainActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        isShowToolbar(false);
         setContentView(R.layout.activity_welcome);
-        mHandler.sendEmptyMessageDelayed(GO_LOGIN, 1500);
+        mHandler.sendEmptyMessageDelayed(1, 1500);
     }
 
-    @SuppressLint("HandlerLeak")
     private Handler mHandler = new Handler() {
 
         @Override
         public void handleMessage(Message msg) {
+            finish();
             startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
         }
 
     };
-
 }
