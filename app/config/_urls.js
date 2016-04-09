97383730506f14bb4f64fbeb47e47103dd85b8bd
杨文锦 <quentin.yang@angejia.com>
2016-04-09 20:55:05 +0800

    change dev to alpha url

--- app/config/_urls.js
+++ app/config/_urls.js
@@ -1,8 +1,8 @@
 'use strict';
 
-let HOST = 'http://360.feature-fy360.dev.angejia.com/service/';
+// let HOST = 'http://360.feature-fy360.dev.angejia.com/service/';
 // let HOST = 'http://360.master.stage.angejia.com/service/';
-// let HOST = 'http://360.feature-fy360.test.angejia.com/service/';
+let HOST = 'http://360.feature-fy360.test.angejia.com/service/';
 // let HOST = 'https://api.fangyuan360.cn/service/';
 
 let urls = {
