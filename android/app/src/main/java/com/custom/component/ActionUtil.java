e4c708067e32cbcc0d4d80f8d6e8ba4aa889e1fe
陈翠卓 <cuizhuochen@angejia.com>
2016-04-15 10:45:55 +0800

    add action log config

--- android/app/src/main/java/com/custom/component/ActionUtil.java
+++ android/app/src/main/java/com/custom/component/ActionUtil.java
@@ -1,6 +1,6 @@
 package com.custom.component;
 
-import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.angejia.android.applog.ActionLog;
 import com.angejia.android.applog.entity.Extend;
@@ -9,6 +9,9 @@ import com.angejia.android.applog.entity.Usage;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableType;
+import com.facebook.react.bridge.ReadableMapKeySetIterator;
 
 import java.util.Iterator;
 
@@ -16,6 +19,8 @@ import java.util.Iterator;
  * Created by angejia on 16/4/12.
  */
 public class ActionUtil extends ReactContextBaseJavaModule {
+    static Usage usageObj = new Usage();
+
     public ActionUtil(ReactApplicationContext reactContext) {
         super(reactContext);
     }
@@ -35,19 +40,106 @@ public class ActionUtil extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public static void setActionWithExtend(String action, String params) {
+    public static void setActionWithExtend(String action, ReadableMap params) {
         Log log = new Log();
         log.setAction(action);
         log.setClickTime(System.currentTimeMillis() / 1000 + "");
 
         Extend extend = new Extend();
-        extend.setCols(params);
+        ReadableMapKeySetIterator r = params.keySetIterator();
+        JSONObject jsonObject = new JSONObject();
+
+        while (r.hasNextKey()){
+            String key = r.nextKey();
+
+            switch (key) {
+                case "vpid":
+                    extend.setVpid(params.getString(key));
+                    break;
+                case "bp":
+                    extend.setBp(params.getString(key));
+                    break;
+                case "aid":
+                    extend.setAid(params.getString(key));
+                    break;
+                case "brokerId":
+                    extend.setBrokerId(params.getString(key));
+                    break;
+                case "visitId":
+                    extend.setVisitId(params.getString(key));
+                    break;
+                case "appOpenTime":
+                    extend.setAppOpenTime(params.getString(key));
+                    break;
+                case "appCloseTime":
+                    extend.setAppCloseTime(params.getString(key));
+                    break;
+                default:
+                    jsonObject.put(key, params.getString(key));
+                    break;
+            }
+        }
+
+        extend.setCols(jsonObject.toString());
         log.setExtend(extend);
 
         ActionLog.setAction(getUsage(), log);
     }
 
+    @ReactMethod
+    public static void setUid(String uid) {
+        usageObj.setUid(uid);
+    }
+    @ReactMethod
+    public static void deleteUid() {
+        usageObj.setUid("");
+    }
+    @ReactMethod
+    public static void setCcid(String ccid) {
+        usageObj.setCcid(ccid);
+    }
+    @ReactMethod
+    public static void setGcid(String gcid) {
+        usageObj.setGcid(gcid);
+    }
+    @ReactMethod
+    public static void setGeo(String geo) {
+        usageObj.setGeo(geo);
+    }
+
+    @ReactMethod
+    public static void setUsage(ReadableMap usageConfig) {
+
+        ReadableMapKeySetIterator r = usageConfig.keySetIterator();
+        String geoStr = "";
+        while (r.hasNextKey()){
+            String key = r.nextKey();
+            switch(key) {
+                case "uid":
+                    usageObj.setUid(usageConfig.getString(key));
+                    break;
+                case "ccid":
+                    usageObj.setCcid(usageConfig.getString(key));
+                    break;
+                case "gcid":
+                    usageObj.setGcid(usageConfig.getString(key));
+                    break;
+                case "lat":
+                    geoStr += usageConfig.getString(key);
+                    break;
+                case "lng":
+                    geoStr += "-" + usageConfig.getString(key);
+                    break;
+                default:
+                    break;
+            }
+        }
+        if(geoStr.length() > 0) {
+            usageObj.setGeo(geoStr);
+        }
+    }
+
     public static Usage getUsage() {
-        return new Usage("当前用户userId", null, null, null);
+        return usageObj;
     }
 }
--- app/utils/ActionLog.js
+++ app/utils/ActionLog.js
@@ -11,10 +11,21 @@
  * 2. function 'setActionWithExtend'
  *
  * parameters
- * a. String action: A string with action num
- * b. String extend: The extend params to send
+ * json: {"vpid": "", "bp": "", "aid": "", "brokerId": "", "visitId": "", "appOpenTime": "", "appCloseTime": "", "other_custum_cols": ""}
+ *
+ * 3. function 'setUsage'
+ *
+ * parameters
+ * json: {"uid": "uid", "ccid": "ccid", "gcid": "gcid", "lat": "lat", "lng": "lng"}
+ *
+ * 4.setUid(String uid)
+ * 5.setCcid(String ccid)
+ * 6.setGcid(String gcid)
+ * 7.setGeo(String geo)  //geo: "lat-lng"
  *
  */
 import { NativeModules } from 'nuke'
+
+//NativeModules.ActionUtil.setUsage({"uid": "uid", "ccid": "ccid", "gcid": "gcid", "lat": "lat", "lng": "lng"});
 
 module.exports = NativeModules.ActionUtil;
