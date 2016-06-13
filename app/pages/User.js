1b4c0100ab4e2d43e22af7e5b1777de6ec2d6e17
储珍珍 <zhenzhenchu@angejia.com>
2016-06-13 21:28:00 +0800

    usercenter basicSection

--- app/pages/User.js
+++ app/pages/User.js
@@ -33,9 +33,29 @@ export default class User extends Component {
     }
 
     render() {
+        let {userProfile} = this.props,
+            mobile = userProfile.get('mobile'),
+            showMobile = mobile ? mobile.slice(0, 3) + '****' + mobile.slice(-4) : '';
+
         return (
             <View style={styles.container}>
                 <Header title='我的' style={styles.bgHeader} fontStyle={styles.whiteText} />
+                <ScrollView
+                    automaticallyAdjustContentInsets={false}
+                >
+                    <View style={styles.basicSection}>
+                        <Image
+                            style={styles.profileAvatar}
+                            source={require('../images/profile.jpg')}
+                        />
+                        <View style={styles.flex}>
+                            <Text style={[styles.mobileText, styles.whiteText]}>{showMobile}</Text>
+                        </View>
+                        <TouchableWithoutFeedback>
+                            <View style={styles.level}><Text style={styles.whiteText}>V1会员</Text></View>
+                        </TouchableWithoutFeedback>
+                    </View>
+                </ScrollView>
             </View>
         );
     }
@@ -46,10 +66,38 @@ const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: '#eee'
     },
+    flex: {
+        flex: 1
+    },
     bgHeader: {
         backgroundColor: '#04c1ae'
     },
     whiteText: {
         color: '#fff'
+    },
+    basicSection: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingTop: 10,
+        paddingBottom: 30,
+        paddingLeft: 20,
+        backgroundColor: '#04c1ae'
+    },
+    profileAvatar: {
+        marginRight: 15,
+        width: 60,
+        height: 60
+    },
+    mobileText: {
+        fontSize: 19
+    },
+    level: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 80,
+        height: 30,
+        backgroundColor: '#ffa251',
+        borderTopLeftRadius: 15,
+        borderBottomLeftRadius: 15
     }
 });
