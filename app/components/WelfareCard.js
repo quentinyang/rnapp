b401f6af20ff7cf2e750e4ff01f728cf8a79cb51
储珍珍 <zhenzhenchu@angejia.com>
2016-06-28 20:58:59 +0800

    welfare

---
+++ app/components/WelfareCard.js
@@ -0,0 +1,96 @@
+'use strict';
+
+import {
+    React,
+    Component,
+    View,
+    Text,
+    Image,
+    StyleSheet,
+    PixelRatio
+} from 'nuke';
+
+export default class Card extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let {errBoxStyle, errTextStyle, errText} = this.props;
+        return (
+            <Image source={require('../images/membership.png')} style={[styles.wfCaSection, styles.center]} resizeMode='stretch'>
+                <View style={styles.wfCaLeft}>
+                    <Text style={styles.highFont}>2</Text><Text style={styles.font12}>积分</Text>
+                </View>
+                <View style={styles.wfSlice}></View>
+                <View style={styles.wfCaDesc}>
+                    <Text style={styles.fontBold}>看房卡</Text>
+                    <Text style={styles.descFont} numberOfLines={1}>· 获任意1套房源的房东电话花1积分</Text>
+                    <Text style={styles.descFont} numberOfLines={1}>· 有效期至2016-07-16</Text>
+                </View>
+                <View style={styles.wfBadge}>
+                    <Image source={require('../images/expired.png')} style={styles.wfBadgeImg} />
+                </View>
+            </Image>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    center: {
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    wfCaSection: {
+        flex: 1,
+        flexDirection: 'row',
+        marginBottom: 15,
+        padding: 10,
+        width: undefined,
+        height: 78,
+        backgroundColor: 'transparent'
+    },
+    wfCaLeft: {
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        marginRight: 10,
+        marginLeft: 8
+    },
+    highFont: {
+        fontSize: 25,
+        color: '#ff6d4b'
+    },
+    font12: {
+        fontSize: 12
+    },
+    wfSlice: {
+        width: 0,
+        height: 58,
+        borderWidth: 1/PixelRatio.get(),
+        borderStyle: 'dashed',
+        borderColor: '#dedede'
+    },
+    wfCaDesc: {
+        flex: 1,
+        paddingLeft: 12,
+    },
+    fontBold: {
+        fontWeight: '600'
+    },
+    descFont: {
+        marginTop: 2,
+        fontSize: 12,
+        color: '#8d8c92'
+    },
+
+    wfBadge: {
+        position: 'absolute',
+        top: 19,
+        right: 20
+    },
+    wfBadgeImg: {
+        width: 40,
+        height: 40,
+        backgroundColor: '#f8f8f8'
+    },
+});
--- app/pages/Welfare.js
+++ app/pages/Welfare.js
@@ -15,6 +15,7 @@ import {
     StyleSheet
 } from 'nuke';
 
+import Card from '../components/WelfareCard';
 let ActionUtil = require( '../utils/ActionLog');
 import * as actionType from '../constants/ActionLog'
 
@@ -33,6 +34,7 @@ export default class Welfare extends Component {
                 <Wtabs current={this.state.current} tabs={tabs} onPress={this.tabClick} />
                 {/*<NoCoupon current={this.state.current} tabs={tabs} />*/}
                 <ScrollView style={styles.container}>
+                    <Card />
                     <Card />
                 </ScrollView>
             </View>
@@ -93,32 +95,6 @@ class NoCoupon extends Component {
     }
 }
 
-class Card extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <Image source={require('../images/membership.png')} style={styles.imgContainer}>
-                <View style={[styles.cardSection, styles.center]}>
-                    <View style={{flexDirection: 'row', alignItems: 'flex-end'}}>
-                        <Text style={styles.importantFont}>2</Text><Text style={styles.font12}>积分</Text>
-                    </View>
-                    <View style={styles.cardDesc}>
-                        <Text style={styles.fontBold}>看房卡</Text>
-                        <Text style={styles.descFont}>· 获任意1套房源的房东电话花1积分</Text>
-                        <Text style={styles.descFont}>· 有效期至2016-07-16</Text>
-                    </View>
-                    <View style={styles.center}>
-                        <Image source={require('../images/used.png')} style={styles.seal} />
-                    </View>
-                </View>
-            </Image>
-        );
-    }
-}
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -157,43 +133,5 @@ const styles = StyleSheet.create({
         marginBottom: 25,
         width: 100,
         height: 67
-    },
-    cardSection: {
-        flex: 1,
-        flexDirection: 'row',
-
-    },
-    importantFont: {
-        fontSize: 25,
-        color: '#ff6d4b'
-    },
-    font12: {
-        fontSize: 12
-    },
-    cardDesc: {
-        paddingLeft: 15,
-        borderWidth: 1,
-
-        borderColor: '#fff',
-        borderStyle: 'dashed'
-    },
-    fontBold: {
-        fontWeight: '600'
-    },
-    descFont: {
-        fontSize: 12,
-        color: '#8d8c92'
-    },
-    seal: {
-        width: 40,
-        height: 40
-    },
-    imgContainer: {
-        flex: 1,
-        width: undefined,
-        height: undefined,
-        paddingLeft: 10,
-        paddingRight: 15,
-        backgroundColor: 'transparent'
     }
 });
