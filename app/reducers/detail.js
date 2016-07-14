168e5a1070f13206db825eda06368fdfdc51f31f
陈翠卓 <cuizhuochen@angejia.com>
2016-07-14 11:54:21 +0800

    detail feedback modal style

--- app/reducers/detail.js
+++ app/reducers/detail.js
@@ -87,7 +87,7 @@ let initParam = {
         msg: '拨打电话失败了,再试一下吧!'
     },
     errorTipVisible: false,
-    feedbackVisible: false,
+    feedbackVisible: true,
     washId: '',
 
     sellerPhone: {
